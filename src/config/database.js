cd9375dd95268d8ad1f8d0bb4e8d468eb6e4a93d
Julián Camilo Villamizar Montañez <111820807+JulianCVM@users.noreply.github.com>
2025-04-29 15:28:02 -0500

    feat: :fire: Eliminacion de archivos
    Eliminacion de archivos innecesarios

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config();
-
-module.exports = {
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'formula1_user',
-    password: process.env.DB_PASSWORD || 'formula1_password',
-    database: process.env.DB_NAME || 'formula1_db',
-    port: process.env.DB_PORT || 3306
-}; 
