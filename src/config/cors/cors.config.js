66b660e8c87a70bd5712d45f0a090fe2e6cf734c
Julián Camilo Villamizar Montañez <111820807+JulianCVM@users.noreply.github.com>
2025-04-30 15:45:12 -0500

    feat: :sparkles: Ajuste de URL
    Se hace el ajuste de la URL con la que se van a recibir las peticiones

--- src/config/cors/cors.config.js
+++ src/config/cors/cors.config.js
@@ -2,7 +2,8 @@ const cors = require('cors');
 
 const corsOptions = {
     // origin: process.env.FRONTEND_URL || 'http://localhost:3000', // URL de tu frontend
-    origin: process.env.FRONTEND_URL || 'http://127.0.0.1:5500', // URL de tu frontend local con live server
+    // origin: process.env.FRONTEND_URL || 'http://127.0.0.1:5500', // URL de tu frontend local con live server
+    origin: process.env.FRONTEND_URL || 'http://localhost:50050', // URL del frontend local con vite desplegado
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
