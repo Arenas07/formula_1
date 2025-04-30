79036057a55e5d1d6481682f8dde78a2865e8126
Julián Camilo Villamizar Montañez <111820807+JulianCVM@users.noreply.github.com>
2025-04-29 19:45:54 -0500

    feat: :sparkles: Actualizacion de variable de entorno
    Se hace una actualizacion a la variable de entorno del cors para su ajuste de ejecucion para que corra con el puerto que ejecuta la extencion de liveserver

--- src/config/cors/cors.config.js
+++ src/config/cors/cors.config.js
@@ -1,7 +1,8 @@
 const cors = require('cors');
 
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:3000', // URL de tu frontend
+    // origin: process.env.FRONTEND_URL || 'http://localhost:3000', // URL de tu frontend
+    origin: process.env.FRONTEND_URL || 'http://127.0.0.1:5500', // URL de tu frontend local con live server
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
